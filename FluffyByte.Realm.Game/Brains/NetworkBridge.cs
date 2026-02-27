388cf6f183d979b6ac0cc0fcd1da8d0280cc51ec
Jacob Chacko <36727167+FluffyByteSoftware@users.noreply.github.com>
2026-02-27 09:45:04 -0700

    Add placeholder classes NetworkBridge and WanderingAI
    Added new source files NetworkBridge.cs and WanderingAi.cs with empty class definitions in their respective namespaces. Both files include metadata headers and licensing notes. These serve as initial placeholders for future implementation.

---
+++ FluffyByte.Realm.Game/Brains/NetworkBridge.cs
@@ -0,0 +1,21 @@
+/*
+ * (NetworkBridge.cs)
+ *------------------------------------------------------------
+ * Created - Thursday, February 26, 2026@5:38:16 PM
+ * Created by - Jacob Chacko
+ *------------------------------------------------------------
+ */
+
+namespace FluffyByte.Realm.Game.Brains;
+
+public class NetworkBridge
+{
+    
+}
+
+/*
+ *------------------------------------------------------------
+ * (NetworkBridge.cs)
+ * See License.txt for licensing information.
+ *-----------------------------------------------------------
+ */
---
+++ FluffyByte.Realm.Game/Entities/Actors/Agents/AI/WanderingAi.cs
@@ -0,0 +1,21 @@
+/*
+ * (WanderingAI.cs)
+ *------------------------------------------------------------
+ * Created - Thursday, February 26, 2026@5:10:51 PM
+ * Created by - Jacob Chacko
+ *------------------------------------------------------------
+ */
+
+namespace FluffyByte.Realm.Game.Entities.Actors.Agents.AI;
+
+public class WanderingAI
+{
+    
+}
+
+/*
+ *------------------------------------------------------------
+ * (WanderingAI.cs)
+ * See License.txt for licensing information.
+ *-----------------------------------------------------------
+ */
